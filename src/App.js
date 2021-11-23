fbd46743b3f8d9e68eb5941cc03f01645bad7b98
momagan <momagan@gmail.com>
2021-11-23 16:48:21 -0700

    🚐 Catching all routes with React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Favetees from './pages/Favetees';
 import Covid19Tracker from './pages/Covid19Tracker';
@@ -12,32 +12,28 @@ import Airbnb from './pages/Airbnb';
 import NotFound from './pages/404';
 import Layout from './components/Layout';
 
-const routes = [
-  { path: '/', component: Home },
-
-  { path: '/Favetees', component: Favetees },
-  { path: '/Covid19Tracker', component: Covid19Tracker },
-  { path: '/Portfolio', component: Portfolio },
-  { path: '/Garabaldi', component: Garabaldi },
-  { path: '/MosDev', component: MosDev },
-  { path: '/Weather', component: Weather },
-  { path: '/Airbnb', component: Airbnb },
-  { path: '*', component: NotFound },
-];
-
 function App() {
-  const routeComponents = routes.map(({ path, component }, key) => (
-    <Route exact path={path} component={component} key={key} />
-  ));
   return (
-    <div className="App">
+    <div className="App" id="#app">
       <Helmet>
         <meta charSet="utf-8" />
         <title>Mo Magan | Portfolio</title>
         <link rel="canonical" href="http://momagan.com" />
       </Helmet>
       <Layout>
-        <Router>{routeComponents}</Router>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Favetees" component={Favetees} />
+            <Route exact path="/Covid19Tracker" component={Covid19Tracker} />
+            <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/Garabaldi" component={Garabaldi} />
+            <Route exact path="/MosDev" component={MosDev} />
+            <Route exact path="/Weather" component={Weather} />
+            <Route exact path="/Airbnb" component={Airbnb} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
       </Layout>
     </div>
   );
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,39 +1,99 @@
-import React, { useRef } from 'react';
+// import React, { useRef } from 'react';
+// // import { NavLink } from 'react-router-dom';
+// import { motion } from 'framer-motion';
+// import Logo from '../../assets/logo_yellow.webp';
+// import Menu from '../Menu/menu';
+// import './navigation.scss';
+
+// const Navigation = () => {
+//   // eslint-disable-next-line no-unused-vars
+//   let navCon = useRef(null);
+//   // eslint-disable-next-line no-unused-vars
+//   let menCon = useRef(null);
+
+//   return (
+//     <motion.div
+//       className="nav"
+//       initial={{ y: -45, opacity: 0 }}
+//       animate={{ y: 0, opacity: 1 }}
+//       transition={{ duration: 1.5, delay: 1.5, ease: [0.6, 0.5, -0.01, 0.9] }}
+//       data-scroll-section
+//     >
+//       <div className="container fluid ">
+//         <div
+//           className="row v-center space-between"
+//           ref={element => (navCon = element)}
+//         >
+//           <div className="logo">
+//             <a href="/" target="Home">
+//               <img src={Logo} alt="Mo Magan ite logo" />
+//             </a>
+//           </div>
+//           <div ref={element => (menCon = element)}>
+//             {/* <button>
+//               <span></span>
+//               <span></span>
+//             </button> */}
+//             <Menu />
+//           </div>
+//         </div>
+//       </div>
+//     </motion.div>
+//   );
+// };
+
+// export default Navigation;
+
+import React, { useEffect } from 'react';
 // import { NavLink } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import Logo from '../../assets/logo_yellow.webp';
 import Menu from '../Menu/menu';
+
+import { useInView } from 'react-intersection-observer';
 import './navigation.scss';
 
 const Navigation = () => {
-  // eslint-disable-next-line no-unused-vars
-  let navCon = useRef(null);
-  // eslint-disable-next-line no-unused-vars
-  let menCon = useRef(null);
+  const animation = useAnimation();
+  const [headRef, inView] = useInView({
+    triggerOnce: false,
+    rootMargin: '-72px',
+  });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start('visible');
+    }
+  }, [animation, inView]);
 
   return (
     <motion.div
+      ref={headRef}
+      animate={animation}
+      initial="hidden"
       className="nav"
-      initial={{ y: -45, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1.5, delay: 1.5, ease: [0.6, 0.5, -0.01, 0.9] }}
       data-scroll-section
+      variants={{
+        visible: {
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 1.2,
+            delay: 0.7,
+            ease: [0.6, 0.5, -0.01, 0.9],
+          },
+        },
+        hidden: { opacity: 0, y: -45 },
+      }}
     >
       <div className="container fluid ">
-        <div
-          className="row v-center space-between"
-          ref={element => (navCon = element)}
-        >
+        <div className="row v-center space-between">
           <div className="logo">
             <a href="/" target="Home">
               <img src={Logo} alt="Mo Magan ite logo" />
             </a>
           </div>
-          <div ref={element => (menCon = element)}>
-            {/* <button>
-              <span></span>
-              <span></span>
-            </button> */}
+          <div>
             <Menu />
           </div>
         </div>
