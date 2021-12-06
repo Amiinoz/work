8d4cb82a01fe33ee3eeaa9af05a5654deba12349
momagan <momagan@gmail.com>
2021-12-06 15:46:43 -0700

    nav animation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,57 +1,37 @@
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import React from 'react';
 import Logo from '../../assets/logo_yellow.webp';
 
-import { useInView } from 'react-intersection-observer';
 import './navigation.scss';
 import Menu from '../Menu/menu';
 
 const Navigation = () => {
-  const animation = useAnimation();
-  const [headRef, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: '-72px',
-  });
-
-  useEffect(() => {
-    if (inView) {
-      animation.start('visible');
-    }
-  }, [animation, inView]);
-
   return (
-    <motion.div
-      ref={headRef}
-      animate={animation}
-      initial="hidden"
-      className="nav"
-      data-scroll-section
-      variants={{
-        visible: {
-          opacity: 1,
-          y: 0,
-          transition: {
-            duration: 1.2,
-            delay: 0.7,
-            ease: [0.6, 0.5, -0.01, 0.9],
-          },
-        },
-        hidden: { opacity: 0, y: -45 },
-      }}
-    >
+    <div className="nav" data-scroll-section>
       <div className="container fluid ">
         <div className="row v-center space-between">
           <div className="logo">
             <a href="/" target="Home">
-              <img src={Logo} alt="Mo Magan site logo" />
+              <img
+                src={Logo}
+                alt="Mo Magan site logo"
+                data-scroll
+                data-scroll-direction="horizontal"
+                data-scroll-speed="-4"
+                data-scroll-position="top"
+              />
             </a>
           </div>
-          <div>
+          <div
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="4"
+            data-scroll-position="top"
+          >
             <Menu />
           </div>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
