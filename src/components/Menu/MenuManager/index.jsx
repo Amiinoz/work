ac38bcfc5873be3c8a8b566ad6ad6397c93bdb00
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2022-12-12 13:46:18 -0700

    Changed project banners

--- src/components/Menu/MenuManager/index.jsx
+++ src/components/Menu/MenuManager/index.jsx
@@ -4,6 +4,7 @@ export const MenuContext = createContext({
   open: false,
   setOpen: () => {},
 });
+console.log(MenuContext);
 
 export default function MenuManager(props) {
   const [open, setOpen] = useState(false);
