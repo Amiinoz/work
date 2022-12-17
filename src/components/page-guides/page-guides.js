1dd1ce6385c9c9448793d210748f637a99d1c8f6
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2022-12-17 10:33:02 -0700

    Refactor css

--- src/components/page-guides/page-guides.js
+++ src/components/page-guides/page-guides.js
@@ -2,20 +2,21 @@ import React from 'react';
 import BackArrow from '../../assets/icons/arrow-right.svg';
 import FrontArrow from '../../assets/icons/arrow-right.svg';
 
-import '../../styles/components/page-guides.scss';
+// import '../../styles/components/page-guides.scss';
+import '../../styles/App.scss';
 
 export default function PageGuides({ nextProject }) {
   return (
     <>
       <div className="page-guides">
-        <div className="backHome">
+        <div className=" guide backHome">
           <a href="/">
             Home
             <img src={BackArrow} alt="Back to Home icon" />
           </a>
         </div>
 
-        <div className="nextProj">
+        <div className=" guide nextProj">
           <a href={nextProject}>
             <img src={FrontArrow} alt="Go to next project" />
             Next Project
